dc827e2d275f464b38c96deae627cd8612481e25
manimoto881985 <manimoto88@gmail.com>
2019-06-26 01:45:13 +0900

    set layout.js

--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,21 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <nav id="navbar" className="navbar ikukyu__navbar">
+      <div className="container ikukyu__container">
+        <div className="navbar-brand">
+          <h1>
+            <Link
+              to="/"
+              className="ikukyu__navbar-item"
+            >
+              <img src="/images/common/title.svg" className="ikukyu__navbar-item__title"/>
+            </Link>
+          </h1>
+        </div>
+      </div>
+    </nav>
   </header>
 )
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "./bulma.min.css"
+import "./common.sass"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -26,21 +27,17 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        <section className="section">
+          <div className="container content ikukyu__container">
+            <main>{children}</main>
+          </div>
+        </section>
+        <footer>
+          <div>© {new Date().getFullYear()} 男性育休やることマニュアル</div>
+          <div>
+            <a href="#">お問い合わせ</a>
+          </div>
+        </footer>
       </>
     )}
   />
