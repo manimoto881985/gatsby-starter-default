29f490586cb22705b376a1d326b8b1f94521d618
manimoto881985 <manimoto88@gmail.com>
2019-06-28 00:27:38 +0900

    トップメニューを全幅化

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,37 +22,43 @@ const IndexPage = () => (
       </div>
     </section>
     <section className="section ikukyu__index__main">
-      <div className="container content ikukyu__container">
+      <div className="container ikukyu__container">
         <div className="ikukyu__index__main__item ikukyu__index__main__item__first">
           <h2 className="ikukyu__index__main__item__heading">体制</h2>
-          <ul>
-            <li>
-              <Link to="/housework">家事分担表の作成</Link>
-            </li>
-            <li>
-              <Link to="/daily_schedule">1日のスケジュール作成</Link>
-            </li>
-            <li>
-              <Link to="/after_childbirth_logistics">出産直後の手続き整理</Link>
-            </li>
-          </ul>
+          <div class="menu">
+            <ul class="menu-list">
+              <li>
+                <Link to="/housework">
+                  家事分担表の作成
+                </Link>
+              </li>
+              <li>
+                <Link to="/daily_schedule">1日のスケジュール作成</Link>
+              </li>
+              <li>
+                <Link to="/after_childbirth_logistics">出産直後の手続き整理</Link>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="ikukyu__index__main__item ikukyu__index__main__item__second">
           <h2 className="ikukyu__index__main__item__heading">買い物</h2>
-          <ul>
-            <li>
-              <Link to="/shopping_list">買い物リストの作成</Link>
-            </li>
-            <li>
-              <Link to="/purchasing_system">日常的な買い物体制の整備</Link>
-            </li>
-            <li>
-              <Link to="/milk_disinfection_baby_bottle">ミルク・消毒・哺乳瓶</Link>
-            </li>
-            <li>
-              <Link to="/stamp">ゴム印の作成</Link>
-            </li>
-          </ul>
+          <div class="menu">
+            <ul class="menu-list">
+              <li>
+                <Link to="/shopping_list">買い物リストの作成</Link>
+              </li>
+              <li>
+                <Link to="/purchasing_system">日常的な買い物体制の整備</Link>
+              </li>
+              <li>
+                <Link to="/milk_disinfection_baby_bottle">ミルク・消毒・哺乳瓶</Link>
+              </li>
+              <li>
+                <Link to="/stamp">ゴム印の作成</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
