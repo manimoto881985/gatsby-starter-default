52067aaa4ba10dfb6892fe3364cd0fd751967904
manimoto881985 <manimoto88@gmail.com>
2019-06-28 00:01:11 +0900

    アイコンを設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#124B7E`,
         theme_color: `#124B7E`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ikukui-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
